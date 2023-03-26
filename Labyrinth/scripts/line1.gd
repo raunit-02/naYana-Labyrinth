6308b71ce77706366212221ed5d4533d2532dd2f
raunit-02 <raunit02@gmail.com>
2023-03-27 01:13:20 +0530

    wordle

---
+++ Labyrinth/scripts/line1.gd
@@ -0,0 +1,45 @@
+extends HBoxContainer
+
+export var line_status  =  false
+
+var word_input:String 
+
+signal word_to_container
+
+func _input(event):
+	if line_status == true:
+		
+		if(
+			 event.is_action_pressed("enter") and
+			word_input.length() == get_child_count()
+			):
+				print("enter")
+				emit_signal("word_to_container", word_input)
+		elif (
+			event.is_action_pressed("backspace") and
+			word_input.length()>0
+			): 
+				word_input.erase(word_input.length()-1,1)
+				update_label()
+				print("backspace")
+		elif( 
+			event is InputEventKey and
+			event.is_echo() == false and
+			event.is_pressed() and
+			event.as_text().length() == 1 and
+			word_input.length() < get_child_count()
+			):
+				word_input += event.as_text()
+				update_label() 
+				get_child(word_input.length() -1).pop_anim()
+				print(word_input)
+
+func update_label():
+	for i in range(get_child_count()):
+		get_child(i).text = ""
+	
+	for i in range(word_input.length()):
+		get_child(i).text = word_input[i]
+
+func _ready():
+	pass 
---
+++ Labyrinth/scripts/wordle.gd
@@ -0,0 +1,13 @@
+extends Control
+
+func _ready():
+	$line_container.connect("game_over",self, "game_over")
+
+
+func game_over(word):
+	$Label.text = word 
+	$AnimationPlayer.play("fade_in")
+	yield(get_tree().create_timer(5),"timeout")
+	get_tree().change_scene("res://scenes/wordle2.tscn")
+
+
