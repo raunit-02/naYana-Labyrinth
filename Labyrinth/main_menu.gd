b73b88563e555bb95bac08bf2123cd503d70deb0
raunit-02 <raunit02@gmail.com>
2023-03-26 19:42:48 +0530

    reorganizing

--- Labyrinth/main_menu.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-
-export var mainGameScene : PackedScene
-
-func _on_NewGameButton_button_up():
-	get_tree().change_scene(mainGameScene.resource_path)
---
+++ Labyrinth/scripts/Namebox.gd
@@ -0,0 +1,18 @@
+extends Control
+
+onready var line_edit = get_node("PlayerName")
+var playerName=" "
+func _ready():
+	line_edit.grab_focus()
+		
+func _on_Button_pressed():
+	playerName = (line_edit.text)
+	queue_free()
+	savepn(playerName)
+	get_tree().change_scene("res://scenes/intro2.tscn")
+
+func savepn(datatosave):
+	var file = File.new()
+	file.open("res://user_data/playerName.txt", File.WRITE)
+	file.store_string(playerName)
+	file.close()
